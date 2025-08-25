a292a4c52c7d9ad4b96040dc020dea28c0812e7c
Shiva Singh Tomar <tomarshiva403@gmail.com>
2025-08-25 16:25:55 +0530

    Updated UserShema

--- models/user.js
+++ models/user.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 
-const userShema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true,
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        lowercase: true, 
+        trim: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     }
-});
+}, { timestamps: true }); 
 
-const User = mongoose.model("user",userShema);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
